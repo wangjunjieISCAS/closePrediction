629e4445e603d0deaa5952b4db11909a3c1e69dc
rahlk <i.m.ralk@gmail.com>
2018-03-30 19:46:15 -0400

    Autocommit

--- python/utils/data.py
+++ python/utils/data.py
@@ -1,7 +1,9 @@
 import pandas as pd 
 from glob2 import glob
+from pdb import set_trace
 
 class DataHandler:
-    def get_data(self, data_dir):
-        files = glob(data_dir, "*.csv")        
+    def get_data(self, path):
+        files = glob(path, "*.csv")        
+        set_trace()
         return 
